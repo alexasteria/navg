04e481a4b811d2e67c1c7b2c3ddbe54ea87b71f0
alexasteria <replay.faq@gmail.com>
2020-03-23 21:20:29 +0300

    Updates

---
+++ precache-manifest.009341df4c1bca07bba405b884a80128.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "075364ad2dc8e33d7910ddae7841d66e",
+    "url": "/navg/index.html"
+  },
+  {
+    "revision": "d25417bd06f22a11beb8",
+    "url": "/navg/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "645809c3f6000dd28b70",
+    "url": "/navg/static/css/main.b8d6b0b9.chunk.css"
+  },
+  {
+    "revision": "d25417bd06f22a11beb8",
+    "url": "/navg/static/js/2.31b74b5c.chunk.js"
+  },
+  {
+    "revision": "f1113b424c2a459239f7b19596860f63",
+    "url": "/navg/static/js/2.31b74b5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "645809c3f6000dd28b70",
+    "url": "/navg/static/js/main.8e3f7127.chunk.js"
+  },
+  {
+    "revision": "e1f12a96addfa902f319",
+    "url": "/navg/static/js/runtime-main.7588a446.js"
+  }
+]);
