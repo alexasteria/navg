d59e6c8bf5d08aa98fb06b6321314bc5cfa693b6
alexasteria <replay.faq@gmail.com>
2020-04-05 22:41:04 +0300

    Updates

---
+++ precache-manifest.635f16b10665bee432c0ee7a5c225ad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "314fb7676b181f0be617610201388841",
+    "url": "/navg/index.html"
+  },
+  {
+    "revision": "c114bdd0e3dd8a815757",
+    "url": "/navg/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "afe1ffcc221858447964",
+    "url": "/navg/static/css/main.162755e4.chunk.css"
+  },
+  {
+    "revision": "c114bdd0e3dd8a815757",
+    "url": "/navg/static/js/2.a826b723.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/navg/static/js/2.a826b723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afe1ffcc221858447964",
+    "url": "/navg/static/js/main.23d539d4.chunk.js"
+  },
+  {
+    "revision": "e1f12a96addfa902f319",
+    "url": "/navg/static/js/runtime-main.7588a446.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/navg/precache-manifest.e27fbcec3b0f51b65637f5d5bdb72a79.js"
+  "/navg/precache-manifest.635f16b10665bee432c0ee7a5c225ad3.js"
 );
 
 self.addEventListener('message', (event) => {
