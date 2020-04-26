d0b7a69bf190cac4aca61a1fae9358d5debba9f0
alexasteria <replay.faq@gmail.com>
2020-04-26 21:26:34 +0300

    Updates

--- precache-manifest.5787b3316c12146ecfc0181560f32e2c.js
+++ precache-manifest.5787b3316c12146ecfc0181560f32e2c.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "76202b32d105d5baa30187a8acf1543c",
+    "revision": "074bd34d46551a123ac60ffd7040172b",
     "url": "/navg/index.html"
   },
   {
-    "revision": "06d90fa94a14fc1df638",
+    "revision": "ba998f57c8cb342a9166",
     "url": "/navg/static/css/2.733ba2d0.chunk.css"
   },
   {
-    "revision": "96a274e9edb2e0211687",
+    "revision": "d2dd7106ae6be551d717",
     "url": "/navg/static/css/main.162755e4.chunk.css"
   },
   {
-    "revision": "06d90fa94a14fc1df638",
-    "url": "/navg/static/js/2.85fe4933.chunk.js"
+    "revision": "ba998f57c8cb342a9166",
+    "url": "/navg/static/js/2.28870ad2.chunk.js"
   },
   {
     "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
-    "url": "/navg/static/js/2.85fe4933.chunk.js.LICENSE.txt"
+    "url": "/navg/static/js/2.28870ad2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "96a274e9edb2e0211687",
-    "url": "/navg/static/js/main.8a6f985b.chunk.js"
+    "revision": "d2dd7106ae6be551d717",
+    "url": "/navg/static/js/main.7aeb23bd.chunk.js"
   },
   {
     "revision": "e1f12a96addfa902f319",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/navg/precache-manifest.f0eb5963926a008940270891711137dc.js"
+  "/navg/precache-manifest.5787b3316c12146ecfc0181560f32e2c.js"
 );
 
 self.addEventListener('message', (event) => {
