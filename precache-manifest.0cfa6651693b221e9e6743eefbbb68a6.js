46cb608fa250639a88fd27ddce2136f589399fe3
alexasteria <replay.faq@gmail.com>
2020-05-25 17:37:15 +0300

    Updates

---
+++ precache-manifest.0cfa6651693b221e9e6743eefbbb68a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df3ade8a387ad594e1b3f65d0d34614",
+    "url": "/navg/index.html"
+  },
+  {
+    "revision": "c20b41234bdbbcad367f",
+    "url": "/navg/static/css/2.47f050ad.chunk.css"
+  },
+  {
+    "revision": "9bc0876eb10f8a929f3d",
+    "url": "/navg/static/css/main.7612df27.chunk.css"
+  },
+  {
+    "revision": "c20b41234bdbbcad367f",
+    "url": "/navg/static/js/2.4dbe1f1c.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/navg/static/js/2.4dbe1f1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bc0876eb10f8a929f3d",
+    "url": "/navg/static/js/main.903ba8a7.chunk.js"
+  },
+  {
+    "revision": "4827bc5e6d71d70614bb",
+    "url": "/navg/static/js/runtime-main.e120f3ba.js"
+  },
+  {
+    "revision": "80eb5c68236314bcf84442d87738f89f",
+    "url": "/navg/static/media/spinner.80eb5c68.svg"
+  }
+]);
