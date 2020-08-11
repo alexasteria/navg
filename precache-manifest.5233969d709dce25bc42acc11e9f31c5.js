a236696f132239d10e8e1b9169eb81b60635cbf0
alexasteria <replay.faq@gmail.com>
2020-08-11 23:51:13 +0300

    Updates

--- precache-manifest.5233969d709dce25bc42acc11e9f31c5.js
+++ precache-manifest.5233969d709dce25bc42acc11e9f31c5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d171eec746fc445ad5b3c8590460665b",
+    "revision": "ee9e79328b82bf7b28517691ede0b1af",
     "url": "/navg/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/navg/static/css/2.47f050ad.chunk.css"
   },
   {
-    "revision": "764701fed8f8b293b45a",
+    "revision": "b707f49a6b99bb890da3",
     "url": "/navg/static/css/main.7612df27.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/navg/static/js/2.28221fb8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "764701fed8f8b293b45a",
-    "url": "/navg/static/js/main.9e7d6472.chunk.js"
+    "revision": "b707f49a6b99bb890da3",
+    "url": "/navg/static/js/main.ab07ff79.chunk.js"
   },
   {
     "revision": "4827bc5e6d71d70614bb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/navg/precache-manifest.b6a403c75436ddf8765e3d0ba3cba823.js"
+  "/navg/precache-manifest.5233969d709dce25bc42acc11e9f31c5.js"
 );
 
 self.addEventListener('message', (event) => {
