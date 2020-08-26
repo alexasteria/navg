4c7f3033b6e9928f1093fd05595534bb109d922f
alexasteria <replay.faq@gmail.com>
2020-08-26 23:48:25 +0300

    Updates

--- precache-manifest.0ad224903d4bd4600cab73d59b256e25.js
+++ precache-manifest.0ad224903d4bd4600cab73d59b256e25.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "77cfb2755877219183e8d974b85bff3f",
+    "revision": "c9b0d809322af9ebeb50b9f70af4f82f",
     "url": "/navg/index.html"
   },
   {
-    "revision": "adfae843c08d41d6724a",
+    "revision": "376f8b488b82c1c88666",
     "url": "/navg/static/css/2.47f050ad.chunk.css"
   },
   {
-    "revision": "f4f834b7773c4f1034d9",
+    "revision": "4b99610d5f673fa592a4",
     "url": "/navg/static/css/main.2a64305e.chunk.css"
   },
   {
-    "revision": "adfae843c08d41d6724a",
-    "url": "/navg/static/js/2.09d7f9b4.chunk.js"
+    "revision": "376f8b488b82c1c88666",
+    "url": "/navg/static/js/2.56670e3d.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/navg/static/js/2.09d7f9b4.chunk.js.LICENSE.txt"
+    "url": "/navg/static/js/2.56670e3d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f4f834b7773c4f1034d9",
-    "url": "/navg/static/js/main.a51282cf.chunk.js"
+    "revision": "4b99610d5f673fa592a4",
+    "url": "/navg/static/js/main.c0240379.chunk.js"
   },
   {
     "revision": "4827bc5e6d71d70614bb",
